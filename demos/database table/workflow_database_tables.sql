466844e58e3622b2df51438fbd060d8711e42312
Brewer Slack <bslack@zagmail.gonzaga.edu>
2018-11-05 14:10:24 -0800

    Update workflow_database_table.sql: add table File_Run implemented changes proposed to Primary Keys

--- demos/database table/workflow_database_tables.sql
+++ demos/database table/workflow_database_tables.sql
@@ -1,31 +1,33 @@
 SET sql_mode = STRICT_ALL_TABLES;
 
 DROP TABLE IF EXISTS Workflow;
-DROP TABLE IF EXISTS Version;
-DROP TABLE IF EXISTS Runs;
+DROP TABLE IF EXISTS Versions;
+DROP TABLE IF EXISTS Run;
 DROP TABLE IF EXISTS Workflow_Data;
 DROP TABLE IF EXISTS Users;
 
 
 CREATE TABLE Workflow(
-	workflow_id INT NOT NULL AUTO_ICREMENT,
+	workflow_id INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY(workflow_id)
 ) ENGINE = InnoDB;
 	
-CREATE TABLE Version(
+CREATE TABLE Versions(
 	version_num INT NOT NULL, 
 	workflow_id INT NOT NULL,
-	PRIMARY KEY(workflow_id),
+	date_modified TIMESTAMP NOT NULL,
+	PRIMARY KEY(workflow_id, version_num),
 	FOREIGN KEY (workflow_id) REFERENCES Workflow(workflow_id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE Runs(
+CREATE TABLE Run(
+	workflow_id INT NOT NULL,
 	version_num INT NOT NULL,
 	run_num INT NOT NULL,
 	graph_output LONGBLOB NOT NULL,
-	recon_output VARCHAR(255) NOT NULL,
-	PRIMARY KEY(version_num),
-	FOREIGN KEY(version_num) REFERENCES Version(version_num)
+	recon_output LONGBLOB NOT NULL,
+	PRIMARY KEY(workflow_id, version_num, run_num),
+	FOREIGN KEY(workflow_id, version_num) REFERENCES Versions(version_num)
 ) ENGINE = InnoDB;
 
 CREATE TABLE Workflow_Data(
@@ -37,9 +39,21 @@ CREATE TABLE Workflow_Data(
 	FOREIGN KEY (workflow_id) REFERENCES Workflow(workflow_id)
 ) ENGINE = InnoDB;
 
+CREATE TABLE File_Run(
+	file_checksum VARCHAR(255) NOT NULL,
+	workflow_id INT NOT NULL,
+	version_num INT NOT NULL,
+	run_num INT NOT NULL,
+	date_modified TIMESTAMP NOT NULL,
+	file_uri VARCHAR(255) NOT NULL,
+	[filename] VARCHAR(255) NOT NULL,
+	PRIMARY KEY(file_checksum,workflow_id,version_num,run_num)
+
+) ENGINE = InnoDB;
+
 CREATE TABLE Users( 
 	username VARCHAR(255) NOT NULL,
-	password VARCHAR(255) NOT NULL,
+	pswd VARCHAR(255) NOT NULL,
 	is_admin BOOL NOT NULL,
 	email VARCHAR(255) NOT NULL,
 	PRIMARY KEY (username)
