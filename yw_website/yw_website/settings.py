e11c3eca2463946eb7a8c90e9235252bb56d5152
Anthony Niehuser <anthonyniehuser@Anthonys-MacBook-Pro.local>
2018-11-05 22:05:40 -0800

    add rest api component and ping route for connection

--- yw_website/yw_website/settings.py
+++ yw_website/yw_website/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'script_upload.apps.ScriptUploadConfig'
-
+    'script_upload.apps.ScriptUploadConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +52,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'yw_website.urls'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 TEMPLATES = [
     {
