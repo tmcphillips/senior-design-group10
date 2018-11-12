932380107aacdcd3e416021f78b01886332cd0aa
Carl <carllundin55@gmail.com>
2018-11-12 14:37:45 -0800

    Added a join table for many-to-many tags. Changed hash field sizes to 128 characters in order to use SHA-512 instead of SHA-1. Changed some date fields to dateTime fields. Changed some varchar attributes to textFields where input size is unkown.

--- yw_website/yw_db/models.py
+++ yw_website/yw_db/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Tag(models.Model):
     parent_tag = models.ForeignKey('self', blank=True, on_delete=models.DO_NOTHING, default=None)
 
+    FILE = "f"
     WORKFLOW = "w"
     VERSION = "v"
     RUN = "r"
@@ -13,6 +14,7 @@ class Tag(models.Model):
         (WORKFLOW, "Workflow"),
         (VERSION, "Version"),
         (RUN, "Run"),
+        (FILE, "File"),
     )
 
     title = models.CharField(max_length=255)
@@ -24,31 +26,27 @@ class WorkFlow(models.Model):
     workflow_tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
 
 class Version(models.Model):
     workflow_id = models.ForeignKey(WorkFlow, on_delete=models.CASCADE)
-    version_tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
-    # TODO: double check checksum is same length as hash length client side
-    script_check_sum = models.CharField(max_length=20)
-    yw_model_check_sum = models.CharField(max_length=20)
+    script_check_sum = models.CharField(max_length=128)
+    yw_model_check_sum = models.CharField(max_length=128)
 
-    yw_model_output = models.CharField(max_length=255)
+    yw_model_output = models.TextField()
     yw_graph_output = models.TextField()
 
-    last_modified = models.DateField()
+    last_modified = models.DateTimeField()
 
 class Run(models.Model):
-    run_tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    version_id = models.ForeignKey(Version, on_delete=models.CASCADE)
 
-    time_stamp = models.DateTimeField()
+    run_time_stamp = models.DateTimeField()
     yw_recon_output = models.TextField()
 
 class File(models.Model):
-    checksum = models.CharField(max_length=20, primary_key=True)
-    file_tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-
+    file_checksum = models.CharField(max_length=128, primary_key=True)
     input_data = models.FileField(upload_to="recon_files/", blank=True)
 
 class RunFile(models.Model):
@@ -57,4 +55,21 @@ class RunFile(models.Model):
 
     uri = models.CharField(max_length=255)
     file_name = models.CharField(max_length=255)
-    time_stamp = models.DateField()
+    last_modified_time_stamp = models.DateTimeField()
+    file_size = models.IntegerField()
+
+class TagWorkflow(models.Model):
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    workflow_id = models.ForeignKey(WorkFlow, on_delete=models.CASCADE)
+
+class TagVersion(models.Model):
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    version_id = models.ForeignKey(Version, on_delete=models.CASCADE)
+
+class TagRun(models.Model):
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    run_id = models.ForeignKey(Run, on_delete=models.CASCADE)
+
+class TagFile(models.Model):
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    file_id = models.ForeignKey(File, on_delete=models.CASCADE)
