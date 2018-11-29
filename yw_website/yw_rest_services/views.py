52f6af221c8832333e73372cb6bdfa34958d6830
Anthony Niehuser <tniehuser@gmail.com>
2018-11-28 17:19:05 -0800

    added a route to handle post request from yw

--- yw_website/yw_rest_services/views.py
+++ yw_website/yw_rest_services/views.py
@@ -1,7 +1,68 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import parser_classes
+from rest_framework.decorators import action
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import viewsets, permissions
+from yw_db.models import *
+from yw_db.serializers import *
+from django.contrib.auth.models import User
+import datetime
+
+
 
 # Create your views here.
 
+@api_view(['get',])
+@permission_classes((permissions.AllowAny,))
 def yw_save_ping(request):
-    return HttpResponse(content=b"You connected to yw web components.")
+    return Response(status=200, data={"data":"You connected to yw web components."})
+
+@api_view(['post'])
+@permission_classes((permissions.AllowAny,))
+def create_workflow(request):
+    username = User.objects.filter(username=request.data.get('username', None))
+    if not username:
+        return Response(status=500, data={'error':'bad username'})
+    username = username[0]
+    w = Workflow(
+        user=User.objects.get(username=username),
+        title=request.data.get('title', 'Title'),
+        description=request.data.get('description','Description')
+    )
+    w.save()
+    v = Version(
+        workflow=Workflow.objects.get(pk=w.id),
+        script_check_sum=request.data.get('script_checksum', ''),
+        yw_model_check_sum=request.data.get('model_checksum', ''),
+        yw_model_output=request.data.get('model', ''),
+        yw_graph_output=request.data.get('graph', ''),
+        last_modified=datetime.datetime.now()
+    )
+    v.save()
+    r = Run(
+        version = Version.objects.get(pk=v.id),
+        yw_recon_output = request.data.get('recon', ''),
+        run_time_stamp = datetime.datetime.now()
+    )
+    r.save()
+    wdata = WorkflowSerializer(w).data
+    wdata['id'] = w.id
+    vdata = VersionSerializer(v).data
+    vdata['id'] = v.id
+    rdata = RunSerializer(r).data
+    rdata['id'] = r.id
+    return Response(status=200, data={
+        "workflow": wdata,
+        "version": vdata,
+        'run': rdata
+        })
+
+    
+ 
+
+
+
--- yw_website/yw_website/settings.py
+++ yw_website/yw_website/settings.py
@@ -28,6 +28,7 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = [
     'workflow-web.gonzaga.edu',
     'localhost',
+    '127.0.0.1',
 ]
 
 
@@ -62,7 +63,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
 }
 
 TEMPLATES = [
