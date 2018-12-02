db70d7bbdfe67b577584d16bbbe2ba487f545e5b
Carl <carllundin55@gmail.com>
2018-12-02 15:02:56 -0800

    Made dummy versions use a workflow graph string

--- yw_website/yw_db/management/commands/create_test_database.py
+++ yw_website/yw_db/management/commands/create_test_database.py
@@ -2,7 +2,42 @@ from autofixture import AutoFixture
 from django.core.management.base import BaseCommand
 from yw_db.models import Workflow, Version, Tag, Run, File, RunFile, TagWorkflow, TagVersion, TagRun, TagFile
 import uuid
+
 TEMP_NUM_ENTRIES = 10
+DUMMY_GRAPH_STRING = """digraph Workflow {
+    rankdir=LR
+    fontname=Helvetica; fontsize=18; labelloc=t
+    label=clean_name_and_date_workflow
+    subgraph cluster_workflow_box_outer { label=""; color=black; penwidth=2
+    subgraph cluster_workflow_box_inner { label=""; penwidth=0
+    node[shape=box style=filled fillcolor="#CCFFCC" peripheries=1 fontname=Helvetica]
+    node[shape=box style=filled fillcolor="#CCFFCC" peripheries=2 fontname=Helvetica]
+    validate_scientificName_field_of_data
+    validate_eventDate_field_of_data
+    edge[fontname=Helvetica]
+    validate_scientificName_field_of_data -> validate_eventDate_field_of_data [label=output1_data]
+    validate_scientificName_field_of_data -> validate_eventDate_field_of_data [label=record_id_data]
+    }}
+    subgraph cluster_input_ports_group_outer { label=""; penwidth=0
+    subgraph cluster_input_ports_group_inner { label=""; penwidth=0
+    node[shape=circle style=filled fillcolor="#FFFFFF" peripheries=1 fontname=Helvetica width=0.2]
+    input1_data_input_port [label=""]
+    }}
+    subgraph cluster_output_ports_group_outer { label=""; penwidth=0
+    subgraph cluster_output_ports_group_inner { label=""; penwidth=0
+    node[shape=circle style=filled fillcolor="#FFFFFF" peripheries=1 fontname=Helvetica width=0.2]
+    name_val_log_output_port [label=""]
+    output2_data_output_port [label=""]
+    date_val_log_output_port [label=""]
+    }}
+    edge[fontname=Helvetica]
+    local_authority_source_input_port -> validate_scientificName_field_of_data [label=local_authority_source]
+    input1_data_input_port -> validate_scientificName_field_of_data [label=input1_data]
+    edge[fontname=Helvetica]
+    validate_scientificName_field_of_data -> name_val_log_output_port [label=name_val_log]
+    validate_eventDate_field_of_data -> output2_data_output_port [label=output2_data]
+    validate_eventDate_field_of_data -> date_val_log_output_port [label=date_val_log]
+    }"""
 
 # TODO: Add file outputs
 # > recon
@@ -10,6 +45,7 @@ TEMP_NUM_ENTRIES = 10
 # > model/graph
 
 class Command(BaseCommand):
+    
     args = '<foo bar ...>'
     help = 'our help string comes here'
 
@@ -22,7 +58,7 @@ class Command(BaseCommand):
         _ = workflow_fixture.create(TEMP_NUM_ENTRIES)
 
     def _create_versions(self):
-        version_fixture = AutoFixture(Version)
+        version_fixture = AutoFixture(Version, field_values={'yw_graph_output': DUMMY_GRAPH_STRING})
         _ = version_fixture.create(TEMP_NUM_ENTRIES)
 
     def _create_runs(self):
