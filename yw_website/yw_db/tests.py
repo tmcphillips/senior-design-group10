ff6ca4c1ed1de5a308d6c065f16eb9d8e31e634c
Carl <carllundin55@gmail.com>
2018-12-06 13:27:59 -0800

    Added test for recieving workflow from client. Added test for database API

--- yw_website/yw_db/tests.py
+++ yw_website/yw_db/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test.client import Client
+from yw_db.models import Workflow
 
 # Create your tests here.
+
+class DBTestCase(TestCase):
+    def test_workflow_save(self):
+        test = Workflow.objects.create(pk=1)
+        c = Client()
+
+        route = "/api/v1/workflows/1/"
+        response = c.get(route)
+
+        self.assertEquals(response.status_code, 200, "Database did not properly populate api")
--- yw_website/yw_rest_services/tests.py
+++ yw_website/yw_rest_services/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
+from yw_db.models import Workflow
 
 # Create your tests here.
 class YwSaveTestCase(TestCase):
@@ -14,3 +16,24 @@ class YwSaveTestCase(TestCase):
         bad_route = '/save/pig/'
         bad_response = self.client.get(path=bad_route)
         self.assertNotEqual(bad_response.status_code, 200, msg="Invalid route failed")
+
+    def test_save_upload(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        route = '/save/'
+
+        data = {}
+        data['username'] = 'testuser'
+        data['title'] = 'test_title'
+        data['description'] = 'test_description'
+        data['model'] = 'test_model'
+        data['model_checksum'] = '2341234123423'
+        data['graph'] = 'test_graph'
+        data['recon'] = 'test_recon'
+
+        response = self.client.post(route, data)
+
+        self.assertEquals(response.status_code, 200, msg="Could not upload a workflow")
+
+        data['username'] = 'nousername'
+        bad_response = self.client.post(route, data)
+        self.assertNotEqual(bad_response.status_code, 200, msg="Bad username unaccounted by save path")
