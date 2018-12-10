ef6e1a0e49e1bde79e245c459c6e50533dfcc902
Carl <carllundin55@gmail.com>
2018-12-10 12:13:38 -0800

    Fixed potential collisions in database rest test

--- yw_website/yw_db/tests.py
+++ yw_website/yw_db/tests.py
@@ -5,11 +5,21 @@ from yw_db.models import Workflow
 # Create your tests here.
 
 class DBTestCase(TestCase):
+    def setUp(self):
+        self.w = Workflow.objects.create()
+        self.w.save()
+
+        
     def test_workflow_save(self):
-        test = Workflow.objects.create(pk=1)
+        test = Workflow.objects.create()
         c = Client()
 
-        route = "/api/v1/workflows/1/"
+        route = "/api/v1/workflows/{}/".format(self.w.id)
+        print(route)
         response = c.get(route)
 
         self.assertEquals(response.status_code, 200, "Database did not properly populate api")
+
+    def tearDown(self):
+        Workflow.delete(self.w)
+        
