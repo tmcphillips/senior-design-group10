1908f37f8460409f92df4ccdec9313a89151e942
Carl <carllundin55@gmail.com>
2019-01-01 22:22:11 -0500

    Added cli options for how many mock entries to generate for testing database.

--- yw_website/yw_db/management/commands/create_test_database.py
+++ yw_website/yw_db/management/commands/create_test_database.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from yw_db.models import Workflow, Version, Tag, Run, File, RunFile, TagWorkflow, TagVersion, TagRun, TagFile
 import uuid
 
-TEMP_NUM_ENTRIES = 10
+# Graphviz output string for an example workflow
 DUMMY_GRAPH_STRING = """digraph Workflow {
     rankdir=LR
     fontname=Helvetica; fontsize=18; labelloc=t
@@ -46,51 +46,57 @@ DUMMY_GRAPH_STRING = """digraph Workflow {
 
 class Command(BaseCommand):
     
-    args = '<foo bar ...>'
-    help = 'our help string comes here'
+    help = 'create_test_database creates mock entries into the YesWorkflow database. This command should only be run in a test environment.'
+    def add_arguments(self, parser):
+        parser.add_argument('--entries', type=int, dest='entries', help='Number of mock entries to create. Default is 10.')
 
     def _create_tags(self):
         tag_fixture = AutoFixture(Tag)
-        _ = tag_fixture.create(TEMP_NUM_ENTRIES)
+        _ = tag_fixture.create(self.num_entries)
 
     def _create_workflows(self):
         workflow_fixture = AutoFixture(Workflow, field_values={'user':None} )
-        _ = workflow_fixture.create(TEMP_NUM_ENTRIES)
+        _ = workflow_fixture.create(self.num_entries)
 
     def _create_versions(self):
         version_fixture = AutoFixture(Version, field_values={'yw_graph_output': DUMMY_GRAPH_STRING})
-        _ = version_fixture.create(TEMP_NUM_ENTRIES)
+        _ = version_fixture.create(self.num_entries)
 
     def _create_runs(self):
         run_fixture = AutoFixture(Run)
-        _ = run_fixture.create(TEMP_NUM_ENTRIES)
+        _ = run_fixture.create(self.num_entries)
 
     def _create_files(self):
-        for i in range(0, TEMP_NUM_ENTRIES):
+        for i in range(0, self.num_entries):
             files_fixture = AutoFixture(File, field_values={'file_checksum':str(uuid.uuid4()), 'input_data':None})
             _ = files_fixture.create_one()
 
     def _create_run_files(self):
         runfile_fixture = AutoFixture(RunFile)
-        _ = runfile_fixture.create(TEMP_NUM_ENTRIES)
+        _ = runfile_fixture.create(self.num_entries)
 
     def _create_tag_workflow(self):
         tagworkflow_fixture = AutoFixture(TagWorkflow)
-        _ = tagworkflow_fixture.create(TEMP_NUM_ENTRIES)
+        _ = tagworkflow_fixture.create(self.num_entries)
 
     def _create_tag_run(self):
         tagrunfixture = AutoFixture(TagRun)
-        _ = tagrunfixture.create(TEMP_NUM_ENTRIES) 
+        _ = tagrunfixture.create(self.num_entries) 
 
     def _create_tag_file(self):
         tagfilefixture = AutoFixture(TagFile)
-        _ = tagfilefixture.create(TEMP_NUM_ENTRIES)
+        _ = tagfilefixture.create(self.num_entries)
 
     def _create_tag_version(self):
         tagversionfixture = AutoFixture(TagVersion)
-        _ = tagversionfixture.create(TEMP_NUM_ENTRIES) 
+        _ = tagversionfixture.create(self.num_entries) 
 
     def handle(self, *args, **options):
+        if type(options['entries']) == int and options['entries'] >= 0:
+           self.num_entries = options['entries']
+        else:
+            self.num_entries = 10
+
         self._create_workflows()
         self._create_tags()
         self._create_versions()
