82d9dcf0b628b09fcaefacedb5a782d2e058f21f
Brooke <bhuntington@zagmail.gonzaga.edu>
2019-01-21 16:50:57 -0800

    dns added to home page

--- yw_website/website/views.py
+++ yw_website/website/views.py
@@ -23,7 +23,8 @@ def home(request):
     paginator = Paginator(documents_list, 10)
     page = request.GET.get('page')
     documents = paginator.get_page(page)
-    return render(request, 'pages/home_page.html', { 'document_list': documents })
+    host = request.get_host()
+    return render(request, 'pages/home_page.html', { 'document_list': documents, 'host': host })
 
 @login_required()
 def my_workflows(request):
