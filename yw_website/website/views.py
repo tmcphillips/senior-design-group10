3918462e4f1c77190c6e93be7d1a1f977b98bbb2
Carl Lundin <carllundin55@gmail.com>
2019-01-30 15:46:29 -0800

    Detail pages (#43)
    * Added route for updating a workflow, as well as some associated tests
    
    * Updated Django due to vulnerabilities
    
    * Removed picture form from detailed workflow. Added workflow ids for updating a workflow.
    
    * Removed json that was accidently added.
    
    * Began refactoring detailed workflow templates
    
    * Added url for versions. Refactored some of the detailed pages html templates
    
    * Removed home button
    
    * Readded detailed css
    
    * Added information for displaying a version
    
    * Began adding a form for selecting a version
    
    * Color changes and small additions
    
    * New dns design & run-detail modularization
    
    * Working form. Need to add URL redirect
    
    * Removed invalid workflows from workflow list
    
    * Added ability to select a version
    
    * Added a complete user authentication ecosystem
    
    * Updated requirements for all-auth
    
    * Added javascript picture of version model to detailed workflow
    
    * Added runs list to detailed worklfow
    
    * Added base template inheritence to run_detail template
    
    * Readded user filtering
    
    * Added new login url to decorator
    
    * Added last modified to list
    
    * css changes
    
    * final color scheme decisions
    
    * Fixed route for updating a workflow
    
    * Added 404 for objects not found, added version to route, and removed a comment block
    
    * Added 404 for objects not found, added version to route, and removed a comment block
    
    * Fixed view endpoint

--- yw_website/website/views.py
+++ yw_website/website/views.py
@@ -1,79 +1,75 @@
-# Create your views here.
+from django import forms
 from django.conf import settings
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout as user_logout
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.storage import FileSystemStorage
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views import generic
 from django.contrib.auth.decorators import login_required
-from website.forms import SignUpForm
 from yw_db.models import Run, Version, Workflow
-
-# from django.contrib.auth.views import password_reset_view
 
 
 def home(request):
-    documents_list = Workflow.objects.all()
-    for document in documents_list:
-        latest_version = Version.objects.filter(workflow_id=document.id).order_by('last_modified').first()
-        document.graph = latest_version.yw_graph_output if latest_version is not None else ""
-    
-    paginator = Paginator(documents_list, 10)
-    page = request.GET.get('page')
-    documents = paginator.get_page(page)
-    host = request.get_host()
-    return render(request, 'pages/home_page.html', { 'document_list': documents, 'host': host })
+    workflow_list = Workflow.objects.all().exclude(version__isnull=True)
+    for workflow in workflow_list:
+        latest_version = Version.objects.filter(workflow=workflow).order_by('last_modified').first()
+        # TODO: every workflow should have at least one version
+        if latest_version is None:
+            workflow_list = workflow_list.exclude(pk=workflow.id)
+        else:
+            workflow.graph = latest_version.yw_graph_output
+            workflow.version_id = latest_version.id
+            workflow.version_modified = latest_version.last_modified
 
-@login_required()
+    paginator = Paginator(workflow_list, 10)
+    page = request.GET.get('page')
+    workflows = paginator.get_page(page)
+    host = request.get_host()
+
+    return render(request, 'pages/home_page.html', { 'workflow_list': workflows, 'host': host  })
+
+@login_required(login_url='/accounts/login/')
 def my_workflows(request):
-    documents_list = Workflow.objects.all().filter(user=request.user)
-    for document in documents_list:
-        latest_version = Version.objects.filter(workflow_id=document.id).order_by('last_modified').first()
-        document.graph = latest_version.yw_graph_output if latest_version is not None else ""
-    
-    paginator = Paginator(documents_list, 10)
+    workflow_list = Workflow.objects.all().filter(user=request.user).exclude(version__isnull=True)
+    for workflow in workflow_list:
+        latest_version = Version.objects.filter(workflow=workflow).order_by('last_modified').first()
+        if latest_version is None:
+            workflow_list = workflow_list.exclude(pk=workflow.id)
+        else:
+            workflow.graph = latest_version.yw_graph_output
+            workflow.version_id = latest_version.id
+
+    paginator = Paginator(workflow_list, 10)
     page = request.GET.get('page')
-    documents = paginator.get_page(page)
+    workflows = paginator.get_page(page)
     host = request.get_host()
-    return render(request, 'pages/my_workflows.html', { 'document_list': documents, 'host': host })
 
-def detailed_workflow(request, document_id):
+    return render(request, 'pages/home_page.html', { 'workflow_list': workflows, 'host': host  })
+def detailed_workflow(request, workflow_id, version_id):
     try:
-        # TODO: change to get object or 404
         if request.method == "GET":
-            document = Workflow.objects.get(pk=document_id)
-            info = {'document': document}
-            return render(request, 'pages/detailed_workflow.html', info)
-    except Workflow.DoesNotExist:
-      return redirect(home)
+            form = request.POST
+            workflow = Workflow.objects.get(pk=workflow_id)
+            version = Version.objects.get(pk=version_id)
+            versions = Version.objects.filter(workflow=workflow)
 
-def run_detail(request):
-    document = Run.objects.get(id="1")
+            runs = Run.objects.filter(version=version)
+            info = {'workflow': workflow, 'version': version, 'versions':versions ,'run_list':runs, 'form': form}
+            return render(request, 'pages/detailed_workflow.html', info)
+        elif request.method == "POST":
+            new_version = request.POST['version_id']
+            return redirect('/detailed_workflow/{}/version/{}/'.format(workflow_id, new_version))
+    
+    except ObjectDoesNotExist:
+        return Response(status=404, data={'error':'workflow not found'})
+
+def run_detail(request, run_id):
+    try:
+        document = Run.objects.get(pk=run_id)
+    except Run.DoesNotExist:
+        return Response(status=404, data={'error':'run not found'})
+
     return render(request, 'pages/run_detail.html', { 'document': document })
 
-def register(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect(home)
-    else:
-        form = SignUpForm()
-    return render(request, 'registration/register.html', {'form': form})
-
-# def forgot_password(request):
-#     if request.method == 'POST':
-#         return password_reset(request, 
-#             from_email=request.POST.get('email'))
-#     else:
-#         return render(request, 'website/forgot_password.html')
-
-def logout(request):
-    user_logout(request)
-    return render(request,'pages/home_page.html')
