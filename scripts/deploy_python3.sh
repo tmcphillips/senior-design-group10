3cb92885cdfb5f4ea8cd76f42230cbc7964ae00f
Carl <carllundin55@gmail.com>
2019-02-06 15:40:18 -0800

    Removed user input in deploy scripts

--- scripts/deploy_python3.sh
+++ scripts/deploy_python3.sh
@@ -8,6 +8,6 @@ python3 manage.py migrate
 python3 manage.py makemigrations website
 python3 manage.py migrate website
 python3 manage.py create_test_database
-python3 manage.py rebuild_index
+python3 manage.py rebuild_index --noinput
 python3 manage.py test
 python3 manage.py runserver
