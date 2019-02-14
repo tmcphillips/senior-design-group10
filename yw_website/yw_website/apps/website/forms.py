1e14eed3452e35d4704f028f8a9d8139512ca989
Carl <carllundin55@gmail.com>
2019-02-13 19:31:48 -0800

    Finished simple search.

--- yw_website/yw_website/apps/website/forms.py
+++ yw_website/yw_website/apps/website/forms.py
@@ -1,7 +1,1 @@
 from django import forms
-from haystack.forms import SearchForm, ModelSearchForm
-from .models import Workflow, Version, Tag, Run, Script, File
-
-class WorkflowSearchForm(SearchForm):
-    def no_query_found(self):
-        return Workflow.objects.all().exclude(version__isnull=True)
