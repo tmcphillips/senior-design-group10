ef38d088adebbf300a037e478272376c2fb599ef
Carl Lundin <carllundin55@gmail.com>
2019-02-20 22:29:06 -0800

    fix for static files not being found from website app (#61)

--- yw_website/yw_website/settings.py
+++ yw_website/yw_website/settings.py
@@ -154,7 +154,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static/'),
+  os.path.join(BASE_DIR, 'yw_website/apps/website/static/'),
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
