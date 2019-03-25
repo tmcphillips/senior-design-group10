0a25508a8a5b917ead35b785848bfaa18abe2534
Carl <carllundin55@gmail.com>
2019-03-25 14:40:19 -0700

    Fixed search and added new data models to search

--- yw_website/yw_website/apps/website/utils.py
+++ yw_website/yw_website/apps/website/utils.py
@@ -4,7 +4,6 @@ from haystack.query import SearchQuerySet
 
 from .models import *
 
-
 def search_and_create_query_set(q):
     """
     This function takes a user search as a parameter and then uses the
@@ -13,28 +12,49 @@ def search_and_create_query_set(q):
     """
     aq = AutoQuery(q)
     sqs = (
-        SearchQuerySet().filter(content=aq).models(Workflow, Version, Tag, Script, Run)
+        SearchQuerySet()
+        .filter(content=aq)
+        .models(
+            Workflow,
+            Version,
+            Tag,
+            Script,
+            ProgramBlock,
+            Data,
+            Port,
+            UriVariable,
+            Resource,
+            UriVariableValue,
+        )
     )
+
     workflows = set()
 
     for result in sqs:
         if result.model_name == "workflow":
             workflows.add(result.object.pk)
         elif result.model_name == "version":
-            workflows.add(result.workflow_id)
+            workflows.add(result.object.workflow.pk)
         elif result.model_name == "run":
             version = Version.objects.get(pk=result.version_id)
             workflows.add(version.workflow_id)
-        elif result.model_name == "tag":
-            pass
-            # TODO: Add support for WorkflowTag in rest API so workflows have a relation to tags
-            # tag_workflows = Workflow.objects.all().select_related("tag").filter(pk=results.pk)
-            # for item in tag_workflows:
-            #     workflows.add(item.pk)
-        elif result.model_name == "script":
-            version = Version.objects().get(pk=result.version_id)
-            workflows.add(version.workflow_id)
-        # TODO: Add searching to files
+        elif result.model_name == "tag" or result.model_name == "script":
+            tag = Tag.objects.get(pk=result.pk)
+            workflow_tags = TagWorkflow.objects.filter(tag=tag)
+            for tag_workflow in workflow_tags:
+                if tag_workflow.workflow.pk not in workflows:
+                    workflows.add(tag_workflow.workflow.pk)
+        elif (
+            result.model_name == "programblock"
+            or result.model_name == "data"
+            or result.model_name == "port"
+            or result.model_name == "urivariable"
+            or result.model_name == "resource"
+            or result.model_name == "urivariablevalue"
+        ):
+            run = Run.objects.get(pk=result.object.run.pk)
+            workflows.add(run.version.workflow.pk)
+
     queries = [Q(pk=key) for key in workflows]
     if len(queries) > 0:
         query = queries.pop()
