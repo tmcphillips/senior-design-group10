e8e00f72c67e70c948257a7cd48a2905a4cc430a
Anthony Niehuser <tniehuser@gmail.com>
2019-03-31 23:23:48 -0700

    delete

--- yw_website/staticfiles/scripts/graphviz_render.js
+++
@@ -1,6 +0,0 @@
-console.log(graph)
-console.log(options)
-
-d3.select(`#graph`)
-    .graphviz(options)
-    .renderDot(graph)
--- yw_website/staticfiles/scripts/home_render_options.js
+++
@@ -1,7 +0,0 @@
-let options = {
-    "zoom":true,
-    "tweenPaths":false,
-    "tweenShapes":false,
-    "convertEqualSidedPolygons":false,
-    "tweenPrecision":10,
-};
