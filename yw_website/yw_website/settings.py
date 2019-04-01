575af4ac5ac505c7ae43183fb2f7d6ea8333abd5
Anthony Niehuser <tniehuser@gmail.com>
2019-03-31 23:30:03 -0700

    actually fixed it

--- yw_website/yw_website/settings.py
+++ yw_website/yw_website/settings.py
@@ -153,10 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'yw_website/apps/website/static/'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'yw_website/apps/website/static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
