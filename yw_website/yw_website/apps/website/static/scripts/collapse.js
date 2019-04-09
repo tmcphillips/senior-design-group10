b13cfd1d78517f46dc8d05c741f0487eeb6af424
aniehuser <33637158+aniehuser@users.noreply.github.com>
2019-04-08 21:10:37 -0700

    Dropdown drops down (#143)
    * inital drop down
    
    * tab
    
    * cleaned up

---
+++ yw_website/yw_website/apps/website/static/scripts/collapse.js
@@ -0,0 +1,37 @@
+let collapseButtons = document.querySelectorAll(".static-left");
+let collapseList = document.querySelector("#collapse")
+
+for(let i = 0; i < collapseButtons.length; i++)
+{
+    // console.log(collapseButtons)
+    let splitDashArray = collapseButtons[i].id.split("-")
+    // console.log("splitting id array: " + splitDashArray)
+    // console.log(splitDashArray)
+    let idNumber = splitDashArray[splitDashArray.length-1]
+    // console.log("getting id num: " + idNumber)
+    let collapseContent = collapseList.querySelector("#collapse-content-" + idNumber)
+    // console.log("getting content from id #collapse-content-" + idNumber + ": ")
+    // console.log(collapseContent)
+    collapseButtons[i].addEventListener("click", (event) => {
+        // console.log("before " + collapseContent.style.display) 
+        // console.log(event)
+        if(!collapseButtons[i].childNodes)
+            return;
+        
+        // console.log("plus minus exists")
+        // console.log(collapseButtons[i].childNodes)
+
+        let plus = collapseButtons[i].querySelector("#expand-button-" + idNumber)
+        let minus = collapseButtons[i].querySelector("#collapse-button-" + idNumber)
+        if(collapseContent.classList.contains("hidden")) {
+            collapseContent.classList.remove("hidden")
+            plus.classList.add("hidden")
+            minus.classList.remove("hidden")
+        } else {
+            collapseContent.classList.add("hidden")
+            plus.classList.remove("hidden")
+            minus.classList.add("hidden")
+        }
+        // console.log("after " + collapseContent.style.display)
+    });
+}
--- yw_website/yw_website/apps/website/templatetags/custom_filters.py
+++ yw_website/yw_website/apps/website/templatetags/custom_filters.py
@@ -3,6 +3,10 @@ from django import template
 register = template.Library()
 
 def trim_url_tail(url):
-    return "/".join(url.split('/')[:-2])
+    return "/".join(url.split("/")[:-2])
 
-register.filter('trim_url_tail', trim_url_tail)
+def trailing_block_name(name):
+    return name.split(".")[-1]
+
+register.filter("trim_url_tail", trim_url_tail)
+register.filter("trailing_block_name", trailing_block_name)
