3335f13b2403be88c89475aa41abb2de194d4075
Anthony Niehuser <tniehuser@gmail.com>
2019-04-09 23:29:11 -0700

    readd comments

--- yw_website/yw_website/apps/website/static/scripts/collapse.js
+++ yw_website/yw_website/apps/website/static/scripts/collapse.js
@@ -1,39 +1,39 @@
 let collapseList = $("#collapse")
 let collapseButtons = collapseList.find(".static-left");
-console.log(collapseButtons)
+// console.log(collapseButtons)
 for(let i = 0; i < collapseButtons.length; i++)
 {
 
     jQueryButton = $(collapseButtons[i])
-    console.log(jQueryButton)
+    // console.log(jQueryButton)
     // console.log(collapseButtons)
     let splitDashArray = jQueryButton.attr("id").split("-")
-    console.log("splitting id array: " + splitDashArray)
+    // console.log("splitting id array: " + splitDashArray)
     // console.log(splitDashArray)
     let idNumber = splitDashArray[splitDashArray.length-1]
-    console.log("getting id num: " + idNumber)
+    // console.log("getting id num: " + idNumber)
     let collapseContent = collapseList.find("#collapse-content-" + idNumber)
     collapseContent.hide()
-    console.log("getting content from id #collapse-content-" + idNumber + ": ")
-    console.log(collapseContent)
+    // console.log("getting content from id #collapse-content-" + idNumber + ": ")
+    // console.log(collapseContent)
 
     let plus = jQueryButton.find("#expand-button-" + idNumber)
     let minus = jQueryButton.find("#collapse-button-" + idNumber)
-    console.log(plus)
-    console.log(minus)
+    // console.log(plus)
+    // console.log(minus)
     minus.hide()
 
     jQueryButton.click((event) => {
-        console.log("before " + idNumber) 
-        console.log(event)
+        // console.log("before " + idNumber) 
+        // console.log(event)
         if(!jQueryButton.children())
             return;
 
         collapseContent.slideToggle(150)
         plus.toggle()
         minus.toggle()
-        console.log("after " + idNumber)
+        // console.log("after " + idNumber)
     });
 
-    console.log(jQueryButton)
+    // console.log(jQueryButton)
 }
