17c923f78c26f29dae57389852aa5032f9124c22
Carl <carllundin55@gmail.com>
2019-04-11 10:11:22 -0700

    Default deploy mode should be no test data generated

--- scripts/deploy_python.sh
+++ scripts/deploy_python.sh
@@ -7,7 +7,7 @@ python manage.py makemigrations
 python manage.py migrate 
 python manage.py makemigrations website
 python manage.py migrate website
-python manage.py create_test_database
+# python manage.py create_test_database
 python manage.py rebuild_index --noinput
 python manage.py test
 python manage.py runserver
