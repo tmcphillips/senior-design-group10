0619a616583d64650a93c371e7922f3ba5e323ec
Brewer Slack <bslack@zagmail.gonzaga.edu>
2019-04-16 20:37:14 -0700

    oops

--- yw_website/yw_website/apps/website/serializers.py
+++ yw_website/yw_website/apps/website/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import *
 import datetime, pytz
-from tclocal import get_localzone
+from tzlocal import get_localzone
 
 from django.core.exceptions import ObjectDoesNotExist
 
