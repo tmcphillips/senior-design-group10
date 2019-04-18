1e21da78bebdce68454663ce3dc1c1d5714efcee
Brewer Slack <bslack@zagmail.gonzaga.edu>
2019-04-17 19:31:47 -0700

    offset not being added

--- yw_website/yw_website/apps/website/serializers.py
+++ yw_website/yw_website/apps/website/serializers.py
@@ -499,14 +499,12 @@ class YesWorkflowSaveSerializer(serializers.ModelSerializer):
             utc = parse(utc)
         print('testing: ', utc)
         if djangotz.is_naive(utc):
-            utc = utc.replace(tzinfo=my_tz_name)
+            utc = utc.astimezone(tzinfo=my_tz_name)
             print('the type: ',type(utc))
             # utc = utc.localize(my_tz_name)
             print('converting naive: ', utc)
         elif djangotz.is_aware(utc):
-            utc = utc.replace(tzinfo=None)
-            utc = utc.replace(tzinfo=my_tz_name)
+            utc = utc.astimezone(tzinfo=my_tz_name)
             print('converting aware: ', utc)
-            # print(utc)
         return utc
 
